3a195697e43fc2a08f46a046f9403eb405752e1c
Girmachew Gulint <girmachewabebe@gmail.com>
2024-04-19 13:36:07 +0000

    env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 let path = require('path')
@@ -7,13 +8,15 @@ app.get('/',(req, res)=>{
     res.send("Hello Express")
 })
 */
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'views', 'index.html'))
 })
 app.get('/json',(req, res)=>{
+    const cs = process.env.MESSAGE_STYLE
     res.status(200).json({
-        message:"Hello json"
+      message:"Hello json".cs
     })
 })
 
