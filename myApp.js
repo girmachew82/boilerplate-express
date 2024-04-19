b912b043149a56c933173474099f0bd43110f452
Girmachew Gulint <girmachewabebe@gmail.com>
2024-04-19 09:08:02 +0000

    ServeranHTMLFile

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
+let path = require('path')
+/*
 console.log("Hello World")
 app.get('/',(req, res)=>{
     res.send("Hello Express")
+})
+*/
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname,'views', 'index.html'))
 })
 
 
