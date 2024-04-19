7b716d001939a484cb8789ef0e3d176dfb3e7d7c
Girmachew Gulint <girmachewabebe@gmail.com>
2024-04-19 12:18:45 +0000

    ServeJSON

--- myApp.js
+++ myApp.js
@@ -11,7 +11,11 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'views', 'index.html'))
 })
-
+app.get('/json',(req, res)=>{
+    res.status(200).json({
+        message:"Hello json"
+    })
+})
 
 
 
