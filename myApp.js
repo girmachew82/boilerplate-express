32cf6ef9d219f51b75f30015b64179081bc946f3
Girmachew Gulint <girmachewabebe@gmail.com>
2024-04-20 12:49:41 +0000

    middleware

--- myApp.js
+++ myApp.js
@@ -8,7 +8,10 @@ app.get('/',(req, res)=>{
     res.send("Hello Express")
 })
 */
-
+app.use(function middleware(req, res, next){
+    console.log(req.method+" "+req.path+ " -"+req.ip)
+    next()
+})
 app.use("/public", express.static(__dirname + "/public"));
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'views', 'index.html'))
@@ -16,9 +19,9 @@ app.get('/',(req, res)=>{
 app.get('/json',(req, res)=>{
     const cs = process.env.MESSAGE_STYLE
     if(cs =="uppercase")
-    response = "Hello josn".toUpperCase()
+    response = "Hello json".toUpperCase()
     else
-    response  = "Hello josn"
+    response  = "Hello json"
     res.status(200).json({
         message : response
     })
